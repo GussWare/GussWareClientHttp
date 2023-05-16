065695eb09f5f8205dc8d39920c9e8f43ae1a459
GussWare <gussware@gmail.com>
2023-05-16 17:43:15 -0600

    chore: cambios en autoload

--- src/ClientHttp.php
+++ src/ClientHttp.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GussWare;
+namespace Gussware;
 
 class ClientHttp
 {
--- src/examples/ajaxPost.php
+++ src/examples/ajaxPost.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use GussWare\ClientHttp;
+use Gussware\Clienthttp;
 
 class AjaxPost
 {
